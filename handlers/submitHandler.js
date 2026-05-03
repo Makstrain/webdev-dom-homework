11941a429e6b3bc4d9a7b5b08dcbe5e3281d5e30
Maksim Ivanichenko <Maxim.Ivanichenko@atom.team>
2026-05-03 10:43:03 +0300

    HW-6 JS ext

--- handlers/submitHandler.js
+++ handlers/submitHandler.js
@@ -1,6 +1,8 @@
 import { postComment, fetchComments } from '../api/commentsApi.js'
 import { renderComments } from '../modules/renderComments.js'
 import { validateForm, resetInputStyles } from '../modules/validateForm.js'
+import { replaceWithMessage } from '../utils/replaceWithMessage.js'
+import { delay } from '../utils/delay.js'
 
 export function initSubmitHandler({
     button,
@@ -21,34 +23,41 @@ export function initSubmitHandler({
             text: commentArea.value,
         }
 
-        // Блокируем кнопку, чтобы не отправляли повторно
-        button.disabled = true
-        button.textContent = 'Отправка...'
+        //    button.disabled = true
+        //    button.textContent = 'Отправка...'
+
+        const loadingSubmit = replaceWithMessage(
+            '.add-form',
+            'Комментарий добавляется...',
+        )
 
         postComment(newComment)
             .then(() => fetchComments())
             .then((freshComments) => {
                 commentsArray.length = 0
                 commentsArray.push(...freshComments)
-
-                // Функция полного обновления
+                return delay(1500)
+            })
+            .then(() => {
+                loadingSubmit.restore()
                 if (onSuccess) {
                     onSuccess()
                 }
-
-                // Очищаем поля
                 nameInput.value = ''
                 commentArea.value = ''
                 resetInputStyles(nameInput, commentArea)
+                button.disabled = false
+                button.textContent = 'Написать'
             })
             .catch((error) => {
                 console.error('Ошибка:', error)
-                alert('Не удалось добавить комментарий. Попробуйте позже.')
-            })
-            .finally(() => {
-                // Разблокируем кнопку
-                button.disabled = false
-                button.textContent = 'Написать'
+                loadingSubmit.updateText('Ошибка при отправке')
+                return delay(2000).then(() => {
+                    loadingSubmit.restore()
+                    alert('Не удалось добавить комментарий. Попробуйте позже.')
+                    button.disabled = false
+                    button.textContent = 'Написать'
+                })
             })
     })
 }
--- script.js
+++ script.js
@@ -6,34 +6,43 @@ import {
 } from './handlers/attachEventHandlers.js'
 import { initSubmitHandler } from './handlers/submitHandler.js'
 import { toggleLike } from './modules/toggleLike.js'
+import { delay } from './utils/delay.js'
+import { replaceWithMessage } from './utils/replaceWithMessage.js'
 
-// Находим элементы на странице
 const nameInput = document.querySelector('.add-form-name')
 const commentArea = document.querySelector('.add-form-text')
 const buttonSubmit = document.querySelector('.add-form-button')
 const commentsList = document.querySelector('.comments')
-
 let comments = []
 
-// Функция полного рендера (отрисовка + навешивание обработчиков)
 function fullRender() {
     renderComments(comments, commentsList, toggleLike, nameInput, commentArea)
     attachLikeHandlers(comments, fullRender)
     attachQuoteHandlers(comments, commentArea, fullRender)
 }
 
-// Загружаем комментарии при старте
+const loadingComments = replaceWithMessage(
+    '.comments',
+    'Комментарии загружаются...',
+)
+
 fetchComments()
     .then((freshComments) => {
         comments.push(...freshComments)
+        return delay(2000)
+    })
+    .then(() => {
+        loadingComments.restore()
         fullRender()
     })
     .catch((error) => {
         console.error('Ошибка загрузки комментариев:', error)
-        alert('Не удалось загрузить комментарии. Обновите страницу.')
+        loadingComments.updateText('Ошибка загрузки комментариев')
+        return delay(3000).then(() => {
+            loadingComments.restore()
+        })
     })
 
-// Инициализируем отправку комментариев
 initSubmitHandler({
     button: buttonSubmit,
     nameInput: nameInput,
